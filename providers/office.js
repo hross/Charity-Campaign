458373739b5366b987a59708a9701884f177c394
hross <hross@hross.net>
2011-09-19 19:06:21 -0400

    added office as object with campaign as parent

---
+++ providers/office.js
@@ -0,0 +1,157 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+var slugify = require('../lib/slugify'); // custom slug tools
+var async = require('async');
+
+OfficeProvider = function(host, port) {
+  this.db= new Db('charity-campaign', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+OfficeProvider.prototype.getNextOfficeId= function(callback) {
+	this.getCounterCollection(function(error, counters) {
+		if( error ) callback(error)
+      	else {
+			counters.findAndModify({_id: "officeId"}, [], {$inc: {c: 1}}, {}, function(error, object) {
+				if (object) {
+					callback(null, object.c + ''); // convert number to string
+				} else {
+					counters.insert({_id: "officeId", c: 1}, function(error, docs) {
+						callback(null, 0 + ''); //convert number to string
+					});
+				}
+			});
+      	}
+    });
+};
+
+OfficeProvider.prototype.getCounterCollection= function(callback) {
+  this.db.collection('counters', function(error, counter_collection) {
+    if( error ) callback(error);
+    else callback(null, counter_collection);
+  });
+};
+
+OfficeProvider.prototype.getCollection= function(callback) {
+  this.db.collection('offices', function(error, office_collection) {
+    if( error ) callback(error);
+    else callback(null, office_collection);
+  });
+};
+
+OfficeProvider.prototype.findAll = function(campaignId, callback) {
+    this.getCollection(function(error, office_collection) {
+      if( error ) callback(error)
+      else {
+        office_collection.find({campaignId: campaignId}).toArray(function(error, results) {
+          if( error ) {
+          	callback(error);
+          } else {
+          	callback(null, results);
+          }
+        });
+      }
+    });
+};
+
+OfficeProvider.prototype.findById = function(id, callback) {
+    this.getCollection(function(error, office_collection) {
+		if (error) {
+      		callback(error)
+      	} else {
+        	//office_collection.db.bson_serializer.ObjectID.createFromHexString(id)
+        	office_collection.findOne({id: id}, function(error, result) {
+				if (error) {
+					console.log(error);
+					callback(error);
+				}
+				callback(null, result);
+			});
+		}
+    });
+};
+
+OfficeProvider.prototype.save = function(offices, callback) {
+	var provider = this;
+    this.getCollection(function(error, office_collection) {
+    	if (error) { callback(error); return; }
+
+		if (typeof(offices.length)=="undefined") offices = [offices];
+
+		var createOffice = function(office, callback) {
+			console.log("creating office...");
+			
+			office.created_at = new Date();
+			office.update_on = new Date();
+			
+			provider.getNextOfficeId(function(error,id) {
+				office.id = id;
+				office.slug = slugify.slugify(office.name);
+				
+				office_collection.insert(office, function() {
+					console.log("created.");
+					callback(null, office);
+				});
+			});
+		};
+		  
+		async.map(offices, createOffice, function(error, results) {
+			if (error) callback(error);
+			
+			// after everything is done we are happy
+			callback(null, results);
+		});
+    });
+};
+
+OfficeProvider.prototype.update = function(offices, callback) {
+	var provider = this;
+    this.getCollection(function(error, office_collection) {
+    	if (error) { callback(error); return; }
+
+		if (typeof(offices.length)=="undefined") offices = [offices];
+
+		var updateOffice = function(office, callback) {
+			office.update_on = new Date();
+			office.slug = slugify.slugify(office.name);
+			
+			office_collection.update({id:office.id}, 
+				{$set: {name: office.name, description: office.description, update_on: office.update_on}},{}, function() {
+				console.log("updated.");
+				callback(null, office);
+			});
+		};
+		  
+		async.map(offices, updateOffice, function(error, results) {
+			if (error) callback(error);
+			
+			// after everything is done we are happy
+			callback(null, results);
+		});
+    });
+};
+
+OfficeProvider.prototype.remove = function(removeId, callback) {
+	var provider = this;
+    this.getCollection(function(error, office_collection) {
+      if( error ) callback(error)
+      else {
+      	office_collection.findOne({id: removeId}, function(error, result) {
+      		if (result) {
+				office_collection.remove({id:removeId}, {}, function(err) {
+					console.log("removed.");
+					callback(null, result.campaignId);
+				});
+			} else {
+				callback(null, null);
+			}
+		});
+      } // end else
+    });
+};
+
+exports.OfficeProvider = OfficeProvider;
